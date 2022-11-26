c208beb18a05a9f799ccace80a019a3fe9baf1d3
alrydb <alrydb123@protonmail.com>
2022-11-27 00:18:38 +0100

    popup progress

--- src/components/ProjectList.jsx
+++ src/components/ProjectList.jsx
@@ -7,10 +7,21 @@ export default function ProjectList() {
   const [buttonPopup, setButtonPopup] = useState(false)
   const [descritpionPopup, setDescriptionPopup] = useState(['hello world'])
   const [imagePopup, setImagePopup] = useState([''])
+  const [titlePopup, setTitlePopup] = useState([''])
   const [linkPopup, setLinkPopup] = useState([''])
 
 
-  const descriptionVaderApp = "En väderapp skapad för android...";
+  const descriptionVaderApp = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero alias ipsam quaerat nihil natus pariatur consequuntur ea a aspernatur repudiandae Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero alias ipsam quaerat nihil natus pariatur consequuntur ea a aspernatur repudiandae";
+  const descriptionGamesDirectory = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero alias ipsam quaerat nihil natus pariatur consequuntur ea a aspernatur repudiandae Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero alias ipsam quaerat nihil natus pariatur consequuntur ea a aspernatur repudiandae  "
+
+  const titleVaderApp ="Väderapp - Android"
+  const titleGamesDirectory = "The Games Directory - React"
+
+  const imageVaderApp = 'images/projectvaderapp.png'
+  const imageGamesDirectory = 'images/projectgamessite.png'
+
+  const linkVaderApp = "https://github.com/alrydb/VaderApp"
+  const linkGamesDirectory = "https://github.com/alrydb/TheGamesDirectory"
 
   return (
     <div className='projectList' id='projects'>
@@ -18,21 +29,26 @@ export default function ProjectList() {
       
 
 
-      
-      
-
-          <div className='project' onClick={() =>  {setButtonPopup(true); setDescriptionPopup(descriptionVaderApp)}}> <p>Väderapp - Android</p><img src='images/projectvaderapp.png' alt="" /> </div>
-          <div className='project' onClick={() => { setButtonPopup(true); setDescriptionPopup("Games Directory - React"); setImagePopup('images/projectgamessite.png')}}> <p>Games Directory - React</p><img src='images/projectgamessite.png' alt="" /> </div>
+          <div className='project' onClick={() =>  {setButtonPopup(true); setTitlePopup(titleVaderApp);setDescriptionPopup(descriptionVaderApp); setImagePopup(imageVaderApp); setLinkPopup(linkVaderApp)}}> <p>{titleVaderApp}</p><img src={imageVaderApp} alt="" /> </div>
+          <div className='project' onClick={() => { setButtonPopup(true); setTitlePopup(titleGamesDirectory);setDescriptionPopup(descriptionGamesDirectory); setImagePopup(imageGamesDirectory); setLinkPopup(linkGamesDirectory)}}> <p>{titleGamesDirectory}</p><img src={imageGamesDirectory}alt="" /> </div>
           <div className='project'> <img src="https://via.placeholder.com/150" alt="" /> </div>
           <div className='project'> <img src="https://via.placeholder.com/150" alt="" /> </div>
          
       <Popup trigger={buttonPopup} setTrigger={setButtonPopup} >
         
-      <div className='project'> 
-      <p> {descritpionPopup}</p>
-      <img src={imagePopup} alt="" />
+     <div className='popup-top'>
+     <img src={imagePopup} alt="" />
+     </div>
       
+      <div className='popup-bottom'>
+      <p> {titlePopup}</p>
+      <p>{descritpionPopup}</p>
+      <p><a href={linkPopup} target="_blank">Länk till Github repo</a></p>
       </div>
+      
+      
+      
+      
       </Popup> 
 
    
