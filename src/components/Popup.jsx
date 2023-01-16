3e05985fb7dc627fdaf6f8e1e0e3f46f718a1076
alrydb <alrydb123@protonmail.com>
2023-01-16 14:15:53 +0100

    j

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 
 function Popup(props) {
   return (props.trigger) ? (
-    <div className='popup'>
-        <div className="popup-inner">
-            <button className='close-btn' onClick={() => props.setTrigger(false)}>close</button>
+    <div className='popup' onClick={() => props.setTrigger(false)}>
+        <div className="popup-inner" onClick={(e) => e.stopPropagation()}>
+            <button className='close-btn' onClick={() => props.setTrigger(false)}>X</button>
             {props.children}
         </div>
 
